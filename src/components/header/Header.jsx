1c6d00d68adc270fd5e8e01877e9574d47577fdb
Sávio Freire <105501774+JSavioFreire@users.noreply.github.com>
2023-02-02 19:08:10 -0300

    iniciando react

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HeaderStyle } from './HeaderStyled'
+
+const Header = () => {
+    return (
+        <HeaderStyle>
+            <h1>teste header</h1>
+        </HeaderStyle>
+    )
+}
+
+export default Header
---
+++ src/global/GlobalStyle.jsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const Global = createGlobalStyle`
+    body{
+        margin: 0;
+        padding: 0;
+        background-color: #2E2D33;
+        color: white;
+    }
+
+
+`
